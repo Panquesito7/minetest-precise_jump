44fcb7f1e0fe183947588fcf3e1d63277b455074
David Leal <halfpacho@gmail.com>
2024-02-02 16:54:40 -0600

    Update screenshot and fix compatibility with older clients (#4)

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 --[[
-    Calculates jump time and reduces the jump height of the player for a more realistic feeling. Built for Minetest.
+    Calculates jump time and adjusts the jump height of the player for a more realistic feel. Built for Minetest.
     Copyright (C) 2023-2024 David Leal (halfpacho@gmail.com) and contributors
 
     This library is free software; you can redistribute it and/or
@@ -21,10 +21,19 @@ local S = minetest.get_translator(minetest.get_current_modname())
 local jump_time, is_holding = { }, { }
 
 minetest.register_privilege("precise_jump", {
-    description = S("Allows the player to perform precise jumps."),
+    description = S("Adjusts the player's jump height according to the jump time for a realistic feel."),
     give_to_singleplayer = false,
     give_to_admin = false
 })
+
+local feat_player_velocity = minetest.features and minetest.features.direct_velocity_on_players
+
+if not feat_player_velocity then
+    minetest.log("warning",
+        "[PRECISE JUMP] Your current Minetest version (MT < 5.4.0) does not support the latest player velocity features." ..
+        "Deprecated callbacks and methods will be used instead to retain compatibility."
+    )
+end
 
 -- Table containing a list of velocities depending on the jump time.
 -- Modifying these values might cause unwanted side effects.
@@ -36,6 +45,19 @@ local velocity_table = {
     {max_time = 1.4, min_time = 0.8, new_velocity_y = -1},
     {max_time = math.huge, min_time = 1.5, new_velocity_y = 0},
 }
+
+--- @brief Adds the given velocity to the player using modern or deprecated methods.
+--- This is to provide full compatibility with MT < 5.4.0 versions.
+--- @param player userdata the player object
+--- @param vec table the velocity vector to be added to the player
+--- @return nil
+local function add_vel(player, vec)
+    if feat_player_velocity then
+       player:add_velocity(vec)
+    else
+        player:add_player_velocity(vector.new(vec.x, vec.y / 1.03, vec.z))
+    end
+end
 
 --- @brief Calculates the time the player is holding the jump button.
 --- @param player userdata The player that will be checked
@@ -62,7 +84,7 @@ local function calculate_time(player)
     else
         for _, v in ipairs(velocity_table) do
             if jump_time[player] <= v.max_time and jump_time[player] >= v.min_time then
-                player:add_velocity(vector.new(0, v.new_velocity_y, 0))
+                add_vel(player, vector.new(0, v.new_velocity_y, 0))
                 break
             end
         end
